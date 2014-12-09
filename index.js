53f09a5c317a4d3fe8e5873afd15d279ba31402b
danderson00 <danderson00@gmail.com>
2014-12-09 14:47:19 +1100

    Added create function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 ﻿var keyPath = require('./keyPath'),
     evaluate = require('./evaluate'),
-    apply = require('./apply');
+    apply = require('./apply'),
+    create = require('./create');
 
 module.exports = {
     evaluateKeyPath: keyPath,
+    setKeyPath: keyPath.set,
     evaluate: evaluate,
-    apply: apply
+    apply: apply,
+    create: create
 };
--- keyPath.js
+++ keyPath.js
@@ -5,3 +5,22 @@
         ? target[path]
         : module.exports(path.substring(index + 1), target[path.substring(0, index)]);
 };
+
+module.exports.set = function (path, target, value) {
+    if (!target) return;
+    var index = path.indexOf('.');
+    if(index === -1)
+        target[path] = value;
+    else {
+        var targetProperty = path.substring(0, index),
+            remainder = path.substring(index + 1),
+            targetObject = target[targetProperty];
+
+        if (!targetObject || typeof targetObject !== 'object') {
+            targetObject = {};
+            target[targetProperty] = targetObject;
+        }
+
+        module.exports.set(remainder, targetObject, value);
+    }
+}
